8b5dd27b9dbe7468769a9e8fbd358d979ae19581
bibekdhkl <bibekdh2016@gmail.com>
2023-09-28 22:47:07 +0545

    UI Updated

--- e-backend/src/middlewares/role.ts
+++
@@ -1,8 +0,0 @@
-import { Response, NextFunction } from 'express';
-import { RoleCode } from '../database/model/Role';
-import { RoleRequest } from 'app-request';
-
-export default (roleCode: RoleCode) => (req: RoleRequest, res: Response, next: NextFunction) => {
-    req.currentRoleCode = roleCode;
-    next();
-};
--- e-backend/src/routes/v1/order/schema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from '@hapi/joi';
-import { JoiObjectId } from '../../../middlewares/validator';
-
-export default {
-  orderId: Joi.object().keys({
-      id: JoiObjectId().required()
-  })
-};
