debd7c19b8f17188ac80b8c05c64fb869d63ab9a
bibekdhkl <bibekdh2016@gmail.com>
2023-09-29 00:38:14 +0545

    Login SignUp Fixed

--- Frontend/src/screens/LoginScreen.js
+++ Frontend/src/screens/LoginScreen.js
@@ -24,11 +24,11 @@ const LoginScreen = ({ location, history }) => {
   }
   return (
     <FormContainer>
-      <h1>SIGN IN</h1>
+      <h1 style={{ textAlign: "center", fontWeight: 800, color: "black" }}>SIGN IN</h1>
       {error && <Message variant='danger'>{error}</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler} className='mt-5'>
-        <Form.Group controlId='email'>
+        <Form.Group controlId='email' style={{ color: "black", fontWeight: 800 }}>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             type='email'
@@ -40,7 +40,7 @@ const LoginScreen = ({ location, history }) => {
         </Form.Group>
 
         <Form.Group controlId='password'>
-          <Form.Label>Password </Form.Label>
+          <Form.Label style={{ color: "black", fontWeight: 800 }}>Password </Form.Label>
           <Form.Control
             type='password'
             placeholder='Enter password'
@@ -49,13 +49,13 @@ const LoginScreen = ({ location, history }) => {
             required
           ></Form.Control>
         </Form.Group>
-        <Button type='submit' variant='primary'>
+        <Button type='submit' variant='primary' style={{fontWeight: 800 }}>
           Sign In
         </Button>
       </Form>{' '}
       <Row className='py-3'>
         <Col>
-          New Customer?
+          New User?
           {/* here redirect is like storing previous request 
           if i am not logged in and then click on add to cart item then it will redirect me to 
           login page and if i am not registerd then i need to register and after registration i will be
